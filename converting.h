773bb26cbe6de93ac050aeed973f6c87662b955d
Linus <klchan11@myseneca.ca>
2021-12-05 12:10:11 -0500

    Add header for converting (module 3) version 1

---
+++ converting.h
@@ -0,0 +1,24 @@
+/*
+Name:         Kin Lok Chan
+Email:        klchan11@myseneca.ca
+ID:           119293215
+Date written: 5 Dec
+Course:       CPR101
+Session:      ZEE
+Project:      CPR101 Final Project
+
+converting.h : CPR101_Project
+
+header file of converting module.
+*/
+
+#ifndef _CONVERTING1_H
+#define _CONVERTING1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void converting();
+
+#endif
